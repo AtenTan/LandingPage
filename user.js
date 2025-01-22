42779ba7ba0f50ec5d8e486b5692e4b7dc8c631d
AtenTan <119356335+AtenTan@users.noreply.github.com>
2025-01-22 10:12:39 +0000

    update v4.1.4

--- user.js
+++ user.js
@@ -2,7 +2,7 @@
 
 function openLinks6() {
     setTimeout(() => {
-      window.open('https://mail.google.com/mail/?view=cm&fs=1&to=atin.tantan@gmail.com&su=Helo%20AtinTan&body=I%27m%20interested%20in%20your%20skills%20as%20a%20Video%20Editor%2C%20can%20we%20chat%20further%3F', '_blank');
+      window.open('https://mail.google.com/mail/?view=cm&fs=1&to=atin.tantan@gmail.com&su=Helo%20AtinTan&body=I%27m%20interested%20on%20your%20skills%20as%20a%20Video%20Editor%2C%20can%20we%20chat%20further%3F', '_blank');
     }, 300);
   }
   
