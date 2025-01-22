5d0d1d6b4014634db250262eb7f297d2cadd4192
AtenTan <119356335+AtenTan@users.noreply.github.com>
2025-01-22 09:11:50 +0000

    update add user.js

---
+++ user.js
@@ -0,0 +1,11 @@
+const menuItems = document.querySelectorAll('.menu__item');
+
+menuItems.forEach((menuItem) => {
+  menuItem.addEventListener('click', (event) => {
+    event.preventDefault();
+    const href = menuItem.getAttribute('href');
+    setTimeout(() => {
+      window.open(href, '_blank');
+    }, 2000);
+  });
+});
