62abd5c4b951f3abd646f1325caa32bedc40ffc3
AtenTan <119356335+AtenTan@users.noreply.github.com>
2025-01-22 10:08:36 +0000

    update v4.1.3

--- user.js
+++ user.js
@@ -2,37 +2,37 @@
 
 function openLinks6() {
     setTimeout(() => {
-      window.open('https://mail.google.com/mail/?view=cm&fs=1&to=atin.tantan@gmail.com&su=Helo AtinTan&body=I%27m%20interested%20in%20your%20skills%20as%20a%20Video%20Editor%2C%20can%20we%20chat%20further%3F', '_blank');
-    }, 1000);
+      window.open('https://mail.google.com/mail/?view=cm&fs=1&to=atin.tantan@gmail.com&su=Helo%20AtinTan&body=I%27m%20interested%20in%20your%20skills%20as%20a%20Video%20Editor%2C%20can%20we%20chat%20further%3F', '_blank');
+    }, 300);
   }
   
   function openLinks5() {
     setTimeout(() => {
       window.open('https://wa.me/6281293305599?text=Helo%20AtinTan.%0AI%27m%20interested%20in%20your%20skills%20as%20a%20Video%20Editor%2C%20can%20we%20chat%20further%3F', '_blank');
-    }, 1000);
+    }, 300);
   }
   
   function openLinks3() {
     setTimeout(() => {
       window.open('https://www.instagram.com/aten.tan/', '_blank');
-    }, 1000);
+    }, 300);
   }
   
   function openLinks4() {
     setTimeout(() => {
       window.open('https://discord.com/users/875703191025627167', '_blank');
-    }, 1000);
+    },300);
   }
     
   function openLinks2() {
     setTimeout(() => {
       window.open('https://atintantan.wixsite.com/mysite/projects', '_blank');
-    }, 1000);
+    }, 300);
   }
     
   function openLinks1() {
     setTimeout(() => {
       window.open('https://www.linkedin.com/in/atintan/', '_blank');
-    }, 1000);
+    }, 300);
   }
 
